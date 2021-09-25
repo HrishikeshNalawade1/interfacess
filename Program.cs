5fcec80eff20fc73e4b722d59701a01b3f8b5366
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-25 08:21:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace interfce2
+{
+    interface ianimal
+    {
+        void sound(string name,string sounds);
+    }
+    interface iaqAnimal:ianimal
+    {
+        void swim(string name,string sounds);
+    }
+    class animals:ianimal,iaqAnimal
+    {
+        public string type="Pet";
+        public string loc="Dip sea";
+        
+
+        public void sound(string name, string sounds)
+        {
+            Console.WriteLine("Name of animal is: "+name+"\nsound produced by him is: "+sounds+"\n Type of Animal is: "+type);
+        }
+        public void swim(string name, string sounds)
+        {
+            Console.WriteLine("Name of acquatic animal is : "+name+"\nsound produced by his is: "+sounds+"\nIt is founf in : "+loc);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            animals a = new animals();
+            a.sound("Dog","bark");
+            a.swim("Shark", "Shrinks");
+        }
+    }
+}
